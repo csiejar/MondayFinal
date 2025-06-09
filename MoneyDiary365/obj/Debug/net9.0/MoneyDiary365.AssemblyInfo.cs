dd5352536aa3cb16fe20365e0251d6704d0d84e5
csiejar <j0970238552@gmail.com>
2025-06-09 13:29:49 +0800

    0609 SQLite 90

--- MoneyDiary365/obj/Debug/net9.0/MoneyDiary365.AssemblyInfo.cs
+++ MoneyDiary365/obj/Debug/net9.0/MoneyDiary365.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoneyDiary365")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e777e10c111998ef05eb1aa16f19d761fe6a638a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be50f7131b0bb940f47796d2c85bca829de37c00")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoneyDiary365")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoneyDiary365")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
